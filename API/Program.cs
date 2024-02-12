bf46e79e57f956b0f7014f11fdf716efb0d8ec32
vermouthyyj <yanyujun666@gmail.com>
2024-02-12 14:57:18 +1100

    fix:remove cors warning

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,15 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpClient(); // Add HttpClient for making HTTP requests
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
