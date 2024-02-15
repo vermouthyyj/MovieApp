4925f4ba7f2944f04148f1545aad7fda913c4215
vermouthyyj <yanyujun666@gmail.com>
2024-02-16 00:19:52 +1100

    fix:remove cors domain restriction

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
 app.MapControllers();
 
 app.Run();
